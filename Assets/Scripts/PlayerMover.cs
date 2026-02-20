6a07e8461eb7871f602b818775d0e35e16cf5493
Hireusen <108818326+Hireusen@users.noreply.github.com>
2026-02-20 17:57:40 +0900

    Init2
    폴더 추가, 씬 추가

---
+++ Assets/Scripts/PlayerMover.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+/// <summary>
+/// 플레이어 오브젝트에 부착하는 C# 스크립트입니다.
+/// </summary>
+public class PlayerMover : MonoBehaviour
+{
+    #region 〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓 인스펙터 〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓
+    [Header("필수 요소 등록")]
+    [SerializeField] private Transform _player;
+
+    [Header("사용자 정의 설정")]
+    [SerializeField] private float _moveSpeed = 10f;
+
+    #endregion
+
+    private void Update()
+    {
+        if (_player == null)
+            return;
+
+        _player.transform.position
+    }
+}
